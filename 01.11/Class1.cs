6f9b0804d1f715608da5788e0a777ce83c6b6ae3
Sabo_cq46@student.itstep.org <Sabo_cq46@student.itstep.org>
2022-11-04 11:28:31 +0200

    Add project files.

---
+++ 01.11/Class1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+    namespace Shapes
+    {
+        abstract class Figure
+        {
+            public abstract string Area();
+            public abstract string Perimeter();
+            public abstract string ShapeName();
+
+            public void ShowInfo()
+            {
+                Console.WriteLine(
+                    $"Name of figure: {ShapeName()}\n" +
+                    $"Area: {Area()}\n" +
+                    $"Perimetr: {Perimeter()}"
+                    );
+                Console.WriteLine();
+            }
+        }
+
+        class Triangle : Figure
+        {
+            double sideA, sideB, sideC;   
+
+            
+            public Triangle(double triangleSideA, double triangleSideB, double triangleSideC)
+            {
+                SideA = triangleSideA;
+                SideB = triangleSideB;
+                SideC = triangleSideC;
+            }
+
+            
+            public double SideA
+            {
+                get { return sideA; }
+                set { sideA = value < 0 ? -value : value; }
+            }
+
+            public double SideB
+            {
+                get { return sideB; }
+                set { sideB = value < 0 ? -value : value; }
+            }
+
+            public double SideC
+            {
+                get { return sideC; }
+                set { sideC = value < 0 ? -value : value; }
+            }
+
+           
+            public override string Area()
+            {
+                double semPer = (sideA + sideB + sideC) / 2;
+                return Math.Sqrt(semPer * (semPer - sideA) * (semPer - sideB) * (semPer - sideC)).ToString();
+            }
+
+          
+            public override string Perimeter()
+            {
+                return (sideA + sideB + sideC).ToString();
+            }
+
+           
+            public override string ShapeName()
+            {
+                return "";
+            }
+        }
+
+        class Square : Figure
+        {
+            double side;   
+
+            
+            public Square(double squareSide)
+            {
+                Side = squareSide;
+            }
+
+           
+            public double Side
+            {
+                get { return side; }
+                set { side = value < 0 ? -value : value; }
+            }
+
+           
+            public override string Area()
+            {
+                return (side * side).ToString();
+            }
+
+           
+            public override string Perimeter()
+            {
+                return (side * 4).ToString();
+            }
+
+            
+            public override string ShapeName()
+            {
+                return "Square";
+            }
+        }
+
+        class Rectangle : Figure
+        {
+            double width;   
+            double height;   
+
+           
+            public Rectangle(double rectangleWidth, double rectangleHeight)
+            {
+                Width = rectangleWidth;
+                Height = rectangleHeight;
+            }
+
+            
+            public double Width
+            {
+                get { return width; }
+                set { width = value < 0 ? -value : value; }
+            }
+
+            public double Height
+            {
+                get { return height; }
+                set { height = value < 0 ? -value : value; }
+            }
+
+            
+            public override string Area()
+            {
+                return (width * height).ToString();
+            }
+
+            
+            public override string Perimeter()
+            {
+                return (width * 2 + height * 2).ToString();
+            }
+
+           
+            public override string ShapeName()
+            {
+                return "Rectangle";
+            }
+        }
+
+        class Program
+        {
+            static void Main()
+            {
+                Figure figure1 = new Triangle(4, 5, 6);
+                figure1.ShowInfo();
+
+                Figure figure2 = new Square(5);
+                figure2.ShowInfo();
+
+                Figure figure3 = new Rectangle(5, 6);
+                figure3.ShowInfo();
+
+                Console.ReadKey();
+            }
+        }
+    }
+
---
+++ 01.11/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+namespace _09_inheritance
+{
+    public class Person
+    {
+        private int id;
+        protected int age;
+        public string Name { get; set; }
+        public DateTime Birthdate { get; set; }
+        public string Country { get; set; }
+
+        public Person() { }
+        public Person(string name)
+        {
+            this.Name = name;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("Birthdate: " + Birthdate.ToLongDateString());
+            Console.WriteLine("Country: " + Country);
+        }
+
+        // you can override virtual methods in the derived classes
+        public virtual void ShowTypeName()
+        {
+            Console.WriteLine("My type is class Person!");
+        }
+    }
+
+    // Derived class can has only one parent class and many interfaces
+    // Inheritance template: class Child : Parent_class, Interface1, Interface2 { }
+    public class Student : Person
+    {
+        public float AverageMark { get; set; }
+        public string Specialisation { get; set; }
+
+        public Student(string name, float avgMark) : base(name)
+        {
+            this.AverageMark = avgMark;
+        }
+        public void ShowBonuses()
+        {
+            if (AverageMark >= 10) Console.WriteLine("Bouses: 100$");
+            else Console.WriteLine("No bonuses!");
+        }
+        public new void ShowInfo()
+        {
+            base.ShowInfo();
+            Console.WriteLine("Average mark: " + AverageMark);
+            Console.WriteLine("Specialisation: " + Specialisation);
+        }
+        public override void ShowTypeName()
+        {
+            Console.WriteLine("My type is class Student!");
+        }
+    }
+
+    public class Employee : Person
+    {
+        public decimal Salary { get; set; }
+
+        public void PayTaxes()
+        {
+            Console.WriteLine("Paid some taxes!");
+        }
+        public override void ShowTypeName()
+        {
+            Console.WriteLine("My type is class Employee!");
+        }
+    }
+
+    public class Manager : Employee
+    {
+        public string ManagementArea { get; set; }
+
+        public override void ShowTypeName()
+        {
+            Console.WriteLine("My type is class Manager!");
+        }
+    }
+
+    class Program
+    {
+        private static void ShowAge(Person person)
+        {
+            // age = current year - birth year 
+            int age = DateTime.Now.Year - person.Birthdate.Year;
+            Console.WriteLine($"{person.Name} has {age} years old.");
+        }
+
+        static void Main(string[] args)
+        {
+            Person p = new Person()
+            {
+                Name = "Bob",
+                Birthdate = new DateTime(1990, 3, 6),
+                Country = "Ukraine"
+            };
+            p.ShowInfo();
+
+            Student st = new Student("Vova", 8.9F)
+            {
+                Birthdate = new DateTime(2004, 1, 17),
+                Country = "Poland",
+                Specialisation = "IT"
+            };
+            st.ShowInfo();
+            st.ShowBonuses();
+
+            Manager m = new Manager()
+            {
+                Birthdate = new DateTime(1988, 3, 6),
+                Name = "Nikolia"
+            };
+            // manager has all the members of the employee and person classes
+
+            ///////////////// new vs override
+            Person person = st;
+
+            Console.WriteLine("----------- [new] vs [override] keywords -----------");
+            person.ShowInfo();      // from Person class (new)
+            person.ShowTypeName();  // from Student class (override)
+
+            ////////////////// array of the classes which based on Person
+            Person[] people = new Person[]
+            {
+                new Person("Vikrotia"),
+                new Student("Oleg", 11.3F),
+                new Employee(),
+                new Manager()
+            };
+
+            foreach (var item in people)
+            {
+                Console.WriteLine("______________________");
+                item.ShowTypeName();
+                item.ShowInfo();
+            }
+
+            ShowAge(m);
+        }
+    }
+}
